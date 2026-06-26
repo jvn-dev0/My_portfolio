165cc26f42d79e2a78f1aa5bdbfda1afd63dbe5e
jeevankb <kbjeevan4334@gmail.com>
2026-06-26 11:18:27 +0530

    Revert to BrowserRouter to fix anchor links

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Admin from './components/Admin';
 import { ThemeProvider } from './ThemeContext';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const Portfolio = () => {
   const pipeRef = React.useRef<HTMLDivElement>(null);
